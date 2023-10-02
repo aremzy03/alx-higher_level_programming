2b3f1c06e78fa2f62245ce6b0395966a94130f38
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-10-02 21:41:02 +0100

    8-concat_edges.py should work now

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str2 = " with Python"
-print(str[39:-63] + str2)
+str = str[39:-63] + str[106:-17] + str[:6]
+print(str[39:-63] + str)
