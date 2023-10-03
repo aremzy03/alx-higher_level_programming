82c60b722f2671373465f3eef0bc7f5cf839df22
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-10-03 13:58:00 +0100

    trying again

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for ascii_val in range(97, 123):
+for ascii_val in range(ord('a'), ord('z') + 1):
     if ascii_val == 101 or ascii_val == 113:
         continue
     print(chr(ascii_val), end="")
