f6aff34d16af092f1e09b24632d73ebc64660d38
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-10-03 22:21:59 +0100

    tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,9 +2,6 @@
 check = 1
 for i in range(ord('z'), ord('a') - 1, -1):
     check += 1
-    if check % 2 == 0:
-        print(chr(i), end="")
-    else:
+    if check % 2 != 0:
         i -= 32
-        print(chr(i), end="")
-        i += 32
+    print(chr(i), end="")
