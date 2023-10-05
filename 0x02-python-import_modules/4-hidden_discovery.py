090f8de4e913182fba0fa1c81e708abdd4c090bc
Abdulmalik Aremu <aremzy2018@gmail.com>
2023-10-05 22:53:33 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,8 @@
 import hidden_4
 if __name__ == "__main__":
     name = dir(hidden_4)
-    for i in name:
-        if name.startswith("__"):
+    for i in sorted(name):
+        if i.startswith("__"):
             continue
         else:
-            print(sorted(name))
+            print(i)
