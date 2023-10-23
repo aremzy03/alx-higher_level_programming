3262af1d27cd97e8fddd9fe0d4263508b9bdc5d3
aremzy03 <aremzy2018@gmail.com>
2023-10-23 22:33:28 +0100

    fixed task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             count += 1
             print(my_list[count], end="")
-        except (IndexError, ValueError, TypeError):
+        except Exception:
             break
     print()
     return (count)
