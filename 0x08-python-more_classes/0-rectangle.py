3c088565d9aab6fa57841803ec3bf04af4a54d80
aremzy03 <aremzy2018@gmail.com>
2023-11-28 17:38:09 +0100

    task 0 to 9 done

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is a rectangle class
+"""
+
+
+class Rectangle:
+    """this is a rectangle class
+    """
+    pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" This is a rectangle module
+"""
+
+
+class Rectangle:
+    """This is a Rectangle class with a getter and setter for the width
+    """
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width < 0 or self.__height < 0:
+            return 0
+        add = self.__width + self.__height
+        return add * 2
