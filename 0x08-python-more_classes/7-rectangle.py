fce32d8a9b303fd5730b3a908972241d59f8b634
aremzy03 <aremzy2018@gmail.com>
2023-11-28 17:44:02 +0100

    fixed a bug

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -55,7 +55,8 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 result += str(self.print_symbol)
-            result += "\n"
+            if i != self.__height - 1:
+                result += "\n"
         return result
 
     def __repr__(self):
